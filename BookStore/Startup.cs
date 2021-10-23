6e0441894eb68c7cc1073fa70b5e3d51b6524dc2
TechyClimate <bijal.techyclimate@gmail.com>
2021-10-23 23:31:00 +0530

    Add MVC Architecture

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -16,24 +16,42 @@ namespace BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline or we define our midleware here.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From my first Middleware\n");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello From my first Middleware response\n");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From my second Middleware\n");
+            //    await next();
+            //    await context.Response.WriteAsync("Hello From my second Middleware response\n");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello From my third Middleware\n");
+            //    await next();
+            //});
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //We can also use MapGet or Only Map the only difeerence between this two methods are
+                //MapGet can handle only Get request which are comming to the perticular route
+                //but Map handles all request which are comming to the perticular route
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
