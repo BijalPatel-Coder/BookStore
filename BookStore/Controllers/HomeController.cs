e8f3ebf8785eb1776475702d1bc59ea4ee41c2f3
TechyClimate <bijal.techyclimate@gmail.com>
2021-10-24 03:29:11 +0530

    Add BookContoller, Repository, Model

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -8,9 +8,13 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello Bijal";
+            return View();
+        }
+        public ViewResult AbouUs()
+        {
+            return View();
         }
     }
 }
---
+++ BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource().ToList();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBooks(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel{id=1,Author="Bijal",Title="MVC Book"},
+                new BookModel{id=2,Author="Bijal",Title="c# Book"},
+                new BookModel{id=3,Author="Piyush",Title="AutoMobileBook"},
+                new BookModel{id=4,Author="Kenisha",Title="CraftBook"},
+                 new BookModel{id=5,Author="Kenisha",Title="FictionStories"},
+            };
+        }
+    }
+}
